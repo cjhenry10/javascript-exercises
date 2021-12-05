c78ee13d552a1e81fe97ad92bd74bf3cd900f86f
Connor Henry <cjhenry10@gmail.com>
2021-12-04 21:36:48 -0500

    exercise 1-7 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
 const reverseString = function(str) {
-
+    let reversed = '';
+    let split = str.split("");
+    let reverseArr = split.reverse();
+    reversed = reverseArr.join("");
+    return reversed;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+    if (num1 < 0 || num2 < 0) return "ERROR";
+    let greater, lesser;
+    if (num1 > num2) {
+        greater = num1;
+        lesser = num2;
+    }
+    else if (num2 > num1) {
+        greater = num2;
+        lesser = num1;
+    }
+    else {
+        return 0;
+    }
+    let sum = 0;
+    for (let i = lesser; i < greater + 1; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
