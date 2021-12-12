7066266bdfae4e9476b9dada9db44f9bb06e6cc3
Connor Henry <cjhenry10@gmail.com>
2021-12-12 12:53:23 -0800

    exercise 11 and 12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let result = {};
+    let oldest = 0;
+    let ageStore = 0;
+    let today = new Date();
+    let year = today.getFullYear();
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].yearOfDeath) {
+            ageStore = arr[i].yearOfDeath - arr[i].yearOfBirth;
+            if (ageStore > oldest) {
+                result = arr[i];
+                oldest = ageStore;
+            }
+        }
+        else {
+            ageStore = year - arr[i].yearOfBirth;
+            if (ageStore > oldest) {
+                result = arr[i];
+                oldest = ageStore;
+            }
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
